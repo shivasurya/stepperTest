b9cd9715d616d334c5f87dfa3afacfe4c6c844ba
s.shivasurya <s.shivasurya@gmail.com>
2016-02-11 23:33:42 +0530

    Added horizontal stepper - brute force design - need to optimize

---
+++ app/src/main/java/stepper/ivb/com/sample/horizontalStepperActivity.java
@@ -0,0 +1,17 @@
+package stepper.ivb.com.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by S.Shivasurya on 2/10/2016 - androidStudio.
+ */
+public class horizontalStepperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_stepper_alternative_base);
+    }
+}
